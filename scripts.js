8d974cd873000999622e9a9f58f2d01a2f65ec9d
Omar Adnan <126019035+OMAR-7WR@users.noreply.github.com>
2024-10-29 03:11:32 +0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -8,7 +8,7 @@ document.getElementById('followForm').addEventListener('submit', function(e) {
     }
 });
 
-const accessToken = 'YOUR_FACEBOOK_ACCESS_TOKEN';
+const accessToken = 'EAAGNO4a7r2wBO5T9z5BNjQWRmwr0w7M60WCUF29Y5eTRoGCxhPugZAipz8TRWel9Eo0xH8Qsums6NPzbJeLkVIu3qFEetLJg3UvngcAjjmZCoigIbknIwLtsMxCDqWXkwq4bQ2q4JGMYlKDKVpvDJevl0A3xAZB602QUdryHhMGA437kf3JZC2mPfK9ixXDAkAZDZD';
 
 function followUser(username) {
     fetch(`https://graph.facebook.com/v12.0/${username}/subscribers?access_token=${accessToken}`, {
